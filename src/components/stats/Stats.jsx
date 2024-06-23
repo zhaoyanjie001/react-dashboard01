0e90fd2f49ea5d57dccb645bfed93a0bff4feb64
zhaoyanjie001 <8324750+zhaoyanjie001@users.noreply.github.com>
2024-06-23 16:57:19 +0900

    update

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,110 +1,105 @@
-import React, { Component } from "react";
+import { Component, React } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-const API_URL = "http://138.138.0.111:8080/api/test/";
+import { MobilePDFReader } from "react-read-pdf";
+import { PDFReader } from "react-read-pdf";
 
-class ListItem extends Component {
-  static contextTypes = {
-    color: PropTypes.string,
-    test: PropTypes.string,
-  };
-  render() {
-    const { value } = this.props;
-    return (
-      <div>
-        <li style={{ background: this.context.color }}>
-          <span>{value}</span>
-        </li>
-        <li style={{ background: this.context.test }}>
-          <span>{value}</span>
-        </li>
-      </div>
-    );
-  }
-}
+import PDFViewer from "pdf-viewer-reactjs";
+
+
+
+import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css"; // Blocks blank TextLayers.
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`; // Fix your issue.
+// <Document file={"sample.pdf"}>
+// <Page
+//   pageNumber={1}
+//   renderTextLayer={false} // Blocks blank TextLayer
+// />
+// </Document>
+
+// import { PDFReader } from 'reactjs-pdf-reader';
+// import { MobilePDFReader } from 'reactjs-pdf-reader';
+
+const API_URL = "http://138.138.0.111:8085/api/test/";
+
+
 
 export default class Stats extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: this.updateGlobalVariable(),
-    };
-  }
-  getUserInfo() {
-    return axios.get(API_URL + "userInfo", {});
+  state = {
+    numPages: 10,
+    pageNumber: 1,
   }
 
-  updateGlobalVariable() {
-    return this.getUserInfo().then(
-      (response) => {
-        this.setState({
-          data: response.data,
-        });
-      },
-      (error) => {
-        this.setState({
-          content:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString(),
-        });
-      }
-    );
-
-    // return this.getUserInfo().then(
-    //   (response) => {
-    //     this.setState({
-    //       content:
-    //         response.data["id"] +
-    //         ":" +
-    //         response.data["username"] +
-    //         ":" +
-    //         response.data["email"] +
-    //         ":" +
-    //         response.data["password"],
-    //       name: response.data["username"],
-    //       password: response.data["password"],
-    //     });
-    //   },
-    //   (error) => {
-    //     this.setState({
-    //       content:
-    //         (error.response && error.response.data) ||
-    //         error.message ||
-    //         error.toString(),
-    //     });
-    //   }
-    // );
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
   }
 
   render() {
-    let userInfo = [];
-    for (let i = 0; i < this.state.data.length; i++) {
-      userInfo.push(<div key={i}>{ "id:" + this.state.data[i]["id"]}       username: {this.state.data[i]["username"]}       email:{this.state.data[i]["email"] } </div>);
-      // userInfo.push(this.state.data[i]["id"]);
-      // console.log(<div key={i}>{this.state.data[i]}</div>)
-    }
-
-    // let list = this.state.data;
-    // list.map((entry, index) => (
-    //   // <ListItem key={`list-${index}`} value={entry.username} />
-    //   console.log(entry)
-    // ))
-
-    // let {content,name,password} = this.stats;
-
-    //  let response = [
-    //   { text: "123", checked: false },
-    //   { text: "321", checked: true },
-    // ];
-    // let response1 = this.updateGlobalVariable();
     return (
       <div>
-        <h2>用户一览</h2>
-
-        {userInfo}
+        <a href={require('./sd.pdf')} target="_self">Download Pdf</a>
       </div>
     );
   }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       {/* <div style={{ overflow: "scroll", height: 600 }}>
+  //         <MobilePDFReader url={"http://localhost:8080/pdf/sample.pdf"} showAllPage="35"/>
+  //       </div> */}
+  //       <PDFViewer
+  //         document={{
+  //           // url: "http://localhost:8080/pdf/sample.pdf",
+           
+  //             url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+  //         }}  
+  //       />
+
+  //     </div>
+  //   );
+  // }
 }
+// export default class Stats extends Component {
+//     constructor(props) {
+//     super(props);
+//     this.state = {
+//       page: 1,
+//       setPage: 1,
+//       canvasRef: null,
+//       file: 'sample.pdf',
+//       pdfDocument: 1,
+//       pdfPage: 1,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         pppp
+//         {!this.state.pdfDocument && <span>Loading...</span>}
+//         <canvas ref={this.state.canvasRef} />
+//         {Boolean(this.state.pdfDocument && this.state.pdfDocument.numPages) && (
+//           <nav>
+//             <ul className="pager">
+//               <li className="previous">
+//                 <button disabled={this.state.page === 1} onClick={() => this.state.setPage(this.state.page - 1)}>
+//                   Previous
+//                 </button>
+//               </li>
+//               <li className="next">
+//                 <button
+//                   disabled={this.state.page === this.state.pdfDocument.numPages}
+//                   onClick={() => this.state.setPage(this.state.page + 1)}
+//                 >
+//                   Next
+//                 </button>
+//               </li>
+//             </ul>
+//           </nav>
+//         )}
+//       </div>
+//     )
+//   }
+// }
